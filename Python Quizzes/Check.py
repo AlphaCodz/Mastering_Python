19a59976d3759a751628633bb2ae49c1a6aa14cf
AlphaCodz <adeyanjuabiona69@gmail.com>
2022-03-09 22:41:07 +0100

    Intro to CSS LEVEL 1

--- Python Quizzes/Check.py
+++ Python Quizzes/Check.py
@@ -1,29 +1,29 @@
 
-import random
-import string
-from tokenize import _all_string_prefixes
+# import random as rand
+# import string as Str
 
-# spam = 7
-# if spam > 5:
-#     print("Five")
-# if spam > 8:
-#     print ("Eight")
+
+spam = 7
+if spam > 5:
+    print("Five")
+if spam > 8:
+    print ("Eight")
     
-    #Answer is 5 because 7 is greater than 5 but lesser than 8. So the First condition is met but the second isn't
+    # Answer is 5 because 7 is greater than 5 but lesser than 8. So the First condition is met but the second isn't
     
-    #Store5 names in an array named Joy and print the array using foreach.
+    # Store5 names in an array named Joy and print the array using foreach.
     
-# Joy = ['Seun', 'Peace', 'Sharon', 'Titilayo', 'Tayo']
-# for friends in Joy:
-#         print(friends)
+Joy = ['Seun', 'Peace', 'Sharon', 'Titilayo', 'Tayo']
+for names in Joy:
+    print(names)
         
 
-# def multiply():
-#     value1 = eval(input("First digit "))
-#     value2 = eval(input("Second digit "))
-#     process = (value1) * (value2)
-#     return process
-# print(multiply())
+def multiply():
+    value1 = eval(input("First digit "))
+    value2 = eval(input("Second digit "))
+    process = (value1) * (value2)
+    return process
+print(multiply())
     
     
     
@@ -32,14 +32,19 @@ from tokenize import _all_string_prefixes
 
 
 
-all_strings = string.ascii_letters+string.digits+string.punctuation
-def RPGenerator():
-    password = ""
-    length = eval(input("Password Length? "))
-   
-    for p in range(length):
-        password += random.choices(all_strings)
-    return password
-print(RPGenerator())
+
+# password = ""
+
+# def random_value(password):
+#     length = int(input("Enter the length of the password >>> "))
+#     counter = 0
     
+#     while counter < length:
+#         password += rand.choice(Str.printable)
+#         counter += 1
+#     print(password)
+    
+# random_value(password)
+    
+#     #SOLVED!
     
