e34d7e10e3bec2a8a1e0848254f8a1b7d05db960
AlphaCodz <adeyanjuabiona69@gmail.com>
2022-03-26 04:17:13 +0100

    Problem solved

---
+++ Mastering_Python/Python Quizzes/Check.py
@@ -0,0 +1,50 @@
+
+# import random as rand
+# import string as Str
+
+
+spam = 7
+if spam > 5:
+    print("Five")
+if spam > 8:
+    print ("Eight")
+    
+    # Answer is 5 because 7 is greater than 5 but lesser than 8. So the First condition is met but the second isn't
+    
+    # Store5 names in an array named Joy and print the array using foreach.
+    
+Joy = ['Seun', 'Peace', 'Sharon', 'Titilayo', 'Tayo']
+for names in Joy:
+    print(names)
+        
+
+def multiply():
+    value1 = eval(input("First digit "))
+    value2 = eval(input("Second digit "))
+    process = (value1) * (value2)
+    return process
+print(multiply())
+    
+    
+    
+#Random Password generator
+
+
+
+
+
+# password = ""
+
+# def random_value(password):
+#     length = int(input("Enter the length of the password >>> "))
+#     counter = 0
+    
+#     while counter < length:
+#         password += rand.choice(Str.printable)
+#         counter += 1
+#     print(password)
+    
+# random_value(password)
+    
+#     #SOLVED!
+    
